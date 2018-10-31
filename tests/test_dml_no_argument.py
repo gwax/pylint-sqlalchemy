edb7b1903279a07dbe54093de774cf0791e348e6
George Leslie-Waksman <waksman@gmail.com>
2018-10-31 00:19:13 -0700

    Add a test.

--- tests/test_dml_no_argument.py
+++ tests/test_dml_no_argument.py
@@ -1,8 +1,42 @@
 """Tests for pylint_sqlalchemy.dml_no_argument"""
+# pylint: disable=redefined-outer-name
 
+import astroid
+from pylint.checkers.typecheck import TypeChecker
+from pylint.testutils import UnittestLinter
 import pytest
 
+import pylint_sqlalchemy
 
-@pytest.mark.skip
-def test_lint_dml_no_argument():
-    """TODO: figure out how to test pylint plugins."""
+
+@pytest.fixture
+def linter():
+    """Fixture providing a testing linter."""
+    pylinter = UnittestLinter()
+    pylint_sqlalchemy.register(pylinter)
+    return pylinter
+
+
+@pytest.fixture
+def checker(linter):
+    """Fixture providing a testing TypeChecker."""
+    checker = TypeChecker(linter)
+    checker.open()
+    return checker
+
+
+def test_dml_no_argument(linter, checker):
+    call = astroid.extract_node(
+        """
+        import sqlalchemy
+
+        TABLE = sqlalchemy.Table(
+            'name', sqlalchemy.MetaData(),
+            sqlalchemy.Column('id', sqlalchemy.Integer),
+        )
+        TABLE.insert()
+        """
+    )
+    checker.visit_call(call)
+
+    assert not linter.release_messages()
